8ec87dadf77ebea0c8e6a31705eb2e612d66f68a
edwinKR <edwin.k.kim@gmail.com>
2019-08-24 14:53:26 -0500

    Done.

---
+++ app/application.rb
@@ -0,0 +1,12 @@
+class Application
+    def call(env)
+        res = Rack::Response.new
+        now = Time.now
+        if  now.hour >= 12
+            res.write "Good Afternoon!"
+        else
+            res.write "Good Morning!"
+        end
+        res.finish
+    end
+end
